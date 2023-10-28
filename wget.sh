4e8f3ed89d1af66554d0240b9f14caa937512525
Spencer Mathews <spencermathews@users.noreply.github.com>
2023-10-27 20:59:42 -0700

    Convert links and back up originals

--- wget.sh
+++ wget.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
 # Same results without trailing slash, but can not reference as index.html.
-wget -N -p -r -l 1 -nH https://bayarea.energyatlas.ucla.edu
+wget -N -p -r -l 1 -k -K -nH https://bayarea.energyatlas.ucla.edu
